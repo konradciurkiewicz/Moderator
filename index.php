03a38c910eac72d217f1832c21e8bc7cde0a958a
Konrad Ciurkiewicz <konradciurkiewicz@wp.pl>
2018-06-07 21:51:59 +0200

    Add PHP Unit

--- index.php
+++ index.php
@@ -1,24 +1,8 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-    </head>
-    <body>
-        <?php
-        require ('vendor/autoload.php');
+<?php
+    require ('vendor/autoload.php');
         
-        use Moderator\lib\File;
+    use Moderator\lib\File;
         
-        $file = new File('http://oleszyce.pl.zetorzeszow.eu/?c=mdGal-cmMiniaturka-0-378-800-600.jpg');
-        $file->exist();
-        $file->available();
-        var_dump(is_dir('http://localhost/Teatr/'));
-        ?>
-    </body>
-</html>
+    $file = new File('http://oleszyce.pl.zetorzeszow.eu/?c=mdGal-cmMiniaturka-0-378-800-600.jpg');
+    $file->exist();
+    $file->available();
